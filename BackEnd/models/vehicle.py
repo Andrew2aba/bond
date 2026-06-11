7391315c44ff7271ce037186c0bb8ade059e8264
Andrew2aba <andrewabarca6@gmail.com>
2026-06-11 15:39:51 -0700

    initial commit

--- BackEnd/models/vehicle.py
+++
@@ -1,9 +0,0 @@
-class Vehicle:
-    def __init__(self, id, displayName, make, model, year, color, vehcleType):
-        self.id = id
-        self.displayName = displayName
-        self.make = make
-        self.model = model
-        self.year = year
-        self.color = color
-        self.vehcleType = vehcleType
